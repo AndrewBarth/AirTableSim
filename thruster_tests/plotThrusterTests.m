8c9196d293a96dcaa515e2f08558b253312963a5
barthal <barthal@mail.uc.edu>
2022-01-10 11:35:49 -0500

    Archive Thruster Test Data
    Create an archive folder to store data from previous thruster tests. Not included in the git database.

--- thruster_tests/plotThrusterTests.m
+++ thruster_tests/plotThrusterTests.m
@@ -1,51 +1,51 @@
 
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_1_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_2_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_3_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_4_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_5_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_6_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_7_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_8_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_9_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_10_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_11_*.mat'));
-% Raspberrypi_MAT_stitcher(dir('ThrusterTest_12_*.mat'));
 
-clear data
+nCase = 30;
+% nCase = 15;
+% nCase = 1;
 
-% data1=load('ThrusterTest_1__stitched.mat');
-% data2=load('ThrusterTest_2__stitched.mat');
-% data3=load('ThrusterTest_3__stitched.mat');
-% data4=load('ThrusterTest_4__stitched.mat');
-% data5=load('ThrusterTest_5__stitched.mat');
-% data6=load('ThrusterTest_6__stitched.mat');
-% data7=load('ThrusterTest_7__stitched.mat');
-% data8=load('ThrusterTest_8__stitched.mat');
-% data9=load('ThrusterTest_9__stitched.mat');
-% data10=load('ThrusterTest_10__stitched.mat');
-% data11=load('ThrusterTest_11__stitched.mat');
-% data12=load('ThrusterTest_12__stitched.mat');
 
-data1=load('ThrusterTest_new_1__stitched.mat');
+clear alldata data
+for i = 1:nCase
+    outFile = strcat('ThrusterTest_',int2str(i),'_*.mat');
+    Raspberrypi_MAT_stitcher(dir(outFile));
+    
+    stFile = strcat('ThrusterTest_',int2str(i),'__stitched.mat');
+    alldata{i} = load(stFile);
 
-% nCase = 12;
-nCase = 1;
+end
+
+
 % Define type of test (1: X trans, 2: Y trans, 3: rot)
-type = [1 1 2 2 3 3 1 1 2 2 3 3];
-
-% data1=load('testUDP_2_1.mat');
-% data2=load('testUDP_2_1.mat');
-% data3=load('testUDP_3_1.mat');
-% data4=load('testUDP_4_1.mat');
-% data5=load('testUDP_5_1.mat');
-% data6=load('testUDP_6_1.mat');
+type = [1 1 2 2 3 3 ...
+        2 2 3 3 1 1 ...
+        1 1 2 2 3 3 ...
+        1 1 2 2 3 3 ...
+        1 1 2 2 3 3];
+% firing_duration = [0.2 0.2 0.2 0.2 0.2 0.2 ...
+%                    0.5 0.5 0.5 0.5 0.5 0.5 ...
+%                    0.5 0.5 0.5 0.5 0.5 0.5 ...
+%                    0.5 0.5 0.5 0.5 0.5 0.5 ...
+%                    0.5 0.5 0.5 0.5 0.5 0.5];
+firing_duration = [0.5 0.5 0.5 0.5 0.5 0.5 ...
+                   1.0 1.0 1.0 1.0 1.0 1.0 ...
+                   0.5 0.5 0.5 0.5 0.5 0.5 ...
+                   0.5 0.5 0.5 0.5 0.5 0.5 ...
+                   0.5 0.5 0.5 0.5 0.5 0.5];
+firing_start =    [2.0 2.0 2.0 2.0 2.0 2.0 ...
+                   2.0 2.0 2.0 2.0 2.0 2.0 ...
+                   2.0 2.0 2.0 2.0 2.0 2.0 ...
+                   2.0 2.0 2.0 2.0 2.0 2.0 ...
+                   2.0 2.0 2.0 2.0 2.0 2.0];
+               
+firing_end = firing_start + firing_duration;
 
 in2m = 0.0254;
 g2mpss = 9.81;
 d2r = pi/180;
 % mass = [11.4]; 
 mass = [19.0];   % kg
+nominalThrust = 0.38;   % N
 
 % Dimensions of sled
 width = 18*in2m;
@@ -57,34 +57,59 @@ inertiaSF = 0.63;
 Izz = 1/12 * mass * (width^2 + depth^2);
 Izz = Izz*inertiaSF;
 
+% Assumption of nominal linear acceleration
+nominalLinAccel = nominalThrust/mass;
+
+% Assumption of nominal angular acceleration
+nominalMoment = nominalThrust*(width/2);
+nominalAngAccel = nominalMoment/Izz; 
+
 % ignore the first second of data
 beginTime = 1;
-startTime = 5;
-endTime = 6;
-% ignore the beginning of the firing
-boffset = 0.4;
+
+% ignore the beginning of the firing (these are percentages of duration)
+boffset = 0.2;  
 eoffset = 0.1;
 
 clear iAccel eAccel dAccel thrust iRate eRate dRate angAccel moment force
-thrust = zeros(nCase,1);
-moment = zeros(nCase,1);
-force = zeros(nCase,1);
+nType = zeros(3,1);
+thrusterList{1} = []; thrusterList{2} = []; thrusterList{3} = []; 
+thrusterList{4} = []; thrusterList{5} = []; thrusterList{6} = [];
+thrusterList{7} = []; thrusterList{8} = [];
+
+typeList{1} = find(type==1);
+typeList{2} = find(type==2);
+typeList{3} = find(type==3);
 for i=1:nCase
     clear filtAccel filtRate
     
-    newVar = strcat('data',int2str(i));
-    data = eval(newVar);
+    data = alldata{i};
 %     filtAccel = squeeze(data.rt_yout.signals(2).values(1,1:3,:));
 %     filtRate  = squeeze(data.rt_yout.signals(2).values(1,4:6,:));
     filtAccel = (data.rt_yout.signals(2).values(:,1:3)*g2mpss)';
     filtRate  = (data.rt_yout.signals(2).values(:,4:6)*d2r)';
     
-    sPt = find(data.rt_tout>startTime,1);
-    ePt = find(data.rt_tout>endTime,1);
-    bPt = find(data1.rt_tout>beginTime,1);
+    thrusterCmds = squeeze(data.rt_yout.signals(1).values(1,:,:))';
+    
+    thrusterOn{i} = zeros(8,1);
+    for j=1:8
+        if ~isempty(find(thrusterCmds(:,j) > 0))
+            % List of thrusters on for each case
+            thrusterOn{i}(j) = 1;
+            % List of cases where each thruster is firing
+            thrusterList{j}{length(thrusterList{j})+1} = i;
+        end
+        
+    end
+    
+    sPt = find(data.rt_tout > firing_start(i),1);
+    ePt = find(data.rt_tout > firing_end(i),1);
+    bPt = find(data.rt_tout>beginTime,1);
     
     basePt = (sPt - bPt)/2;
-    duration = endTime-startTime;
+    startTime = firing_start(i);
+    endTime = firing_end(i);
+    duration = firing_duration(i);
     
     newsPt = find(data.rt_tout>(startTime+duration*boffset),1);
     newePt = find(data.rt_tout>(endTime-duration*eoffset),1);
@@ -92,41 +117,74 @@ for i=1:nCase
     
     
     if type(i) == 1
+        nType(1) = nType(1) + 1;
 %         iAccel(i) = mean(filtAccel(1,bPt:basePt));
-        iAccel(i) = mean(filtAccel(1,bPt:sPt));
-        eAccel(i) = mean(filtAccel(1,newsPt:newePt));
-        dAccel(i) = eAccel(i) - iAccel(i);
-        thrust(i) = dAccel(i)*mass;
+        onAxisAccel = filtAccel(1,:);
+        offAxisAccel = filtAccel(2,:);
+%         iAccelOn{type(i)}{nType(type(i))} = mean(filtAccel(1,bPt:sPt));
+%         eAccelOn{type(i)}{nType(type(i))} = mean(filtAccel(1,newsPt:newePt));
+%         dAccelOn{type(i)}{nType(type(i))} = eAccelOn{type(i)}{nType(type(i))} - iAccelOn{type(i)}{nType(type(i))};
+%         thrustOn{type(i)}{nType(type(i))} = dAccelOn{type(i)}{nType(type(i))}*mass;
+%         
+%         iAccelOff{type(i)}{nType(type(i))} = mean(filtAccel(1,bPt:sPt));
+%         eAccelOff{type(i)}{nType(type(i))} = mean(filtAccel(1,newsPt:newePt));
+%         dAccelOff{type(i)}{nType(type(i))} = eAccelOff{type(i)}{nType(type(i))} - iAccelOff{type(i)}{nType(type(i))};
+%         thrustOff{type(i)}{nType(type(i))} = dAccelOff{type(i)}{nType(type(i))}*mass;
     elseif type(i) == 2
-%         iAccel(i) = mean(filtAccel(2,bPt:basePt));
-        iAccel(i) = mean(filtAccel(2,bPt:sPt));
-        eAccel(i) = mean(filtAccel(2,newsPt:newePt));
-        dAccel(i) = eAccel(i) - iAccel(i);
-        thrust(i) = dAccel(i)*mass;
+        nType(2) = nType(2) + 1;
+        onAxisAccel = filtAccel(2,:);
+        offAxisAccel = filtAccel(1,:);
+% %         iAccel(i) = mean(filtAccel(2,bPt:basePt));
+%         iAccelOn(i) = mean(filtAccel(2,bPt:sPt));
+%         eAccelOn(i) = mean(filtAccel(2,newsPt:newePt));
+%         dAccelOn(i) = eAccelOn(i) - iAccelOn(i);
+%         thrustOn(i) = dAccelOn(i)*mass;
     elseif type(i) == 3
-%         iRate(i) = mean(filtRate(3,bPt:basePt));
-        iRate(i) = mean(filtRate(3,bPt:sPt));
-        eRate(i) = filtRate(3,newePt);
-        dRate(i) = eRate(i) - iRate(i);
-        angAccel(i) = dRate(i)/deltaTime;
-        moment(i) = Izz*angAccel(i);
-        force(i) = moment(i)/(width/2);
+        nType(3) = nType(3) + 1;
+        onAxisAccel = filtAccel(1,:);
+        offAxisAccel = filtAccel(2,:);
+% %         iRate(i) = mean(filtRate(3,bPt:basePt));
+%         iRate(i) = mean(filtRate(3,bPt:sPt));
+%         eRate(i) = filtRate(3,newePt);
+%         dRate(i) = eRate(i) - iRate(i);
+%         angAccel(i) = dRate(i)/deltaTime;
+%         moment(i) = Izz*angAccel(i);
+%         force(i) = moment(i)/(width/2);
     end
+
+    iAccelOn(i) = mean(onAxisAccel(1,bPt:sPt));
+    eAccelOn(i) = mean(onAxisAccel(1,newsPt:newePt));
+    dAccelOn(i) = eAccelOn(i) - iAccelOn(i);
+    thrustOn(i) = dAccelOn(i)*mass;
     
-    figure;
-    subplot(2,1,1);plot(data.rt_tout,filtAccel(1,:))
-    xlabel('Time (sec)');ylabel('Accel (m/s2)');
-    title(['X Linear Acceleration, Case: ' num2str(i)])
-    subplot(2,1,2);plot(data.rt_tout,filtAccel(2,:))
-    xlabel('Time (sec)');ylabel('Accel (m/s2)');
-    title(['Y Linear Acceleration, Case: ' num2str(i)])  
+    iAccelOff(i) = mean(offAxisAccel(1,bPt:sPt));
+    eAccelOff(i) = mean(offAxisAccel(1,newsPt:newePt));
+    dAccelOff(i) = eAccelOff(i) - iAccelOff(i);
+    thrustOff(i) = dAccelOff(i)*mass;
+       
+    iRate(i) = mean(filtRate(3,bPt:sPt));
+    eRate(i) = filtRate(3,newePt);
+    dRate(i) = eRate(i) - iRate(i);
+    angAccel(i) = dRate(i)/deltaTime;
+    moment(i) = Izz*angAccel(i);
+    force(i) = moment(i)/(width/2);
     
-    figure;
-    plot(data.rt_tout,filtRate(3,:)./d2r)
-    xlabel('Time (sec)');ylabel('Rate (deg/s)');
-    title(['Rotational Rate, Case:' num2str(i)])
+%     figure;
+%     subplot(2,1,1);plot(data.rt_tout,filtAccel(1,:))
+%     xlabel('Time (sec)');ylabel('Accel (m/s2)');
+%     title(['X Linear Acceleration, Case: ' num2str(i)])
+%     subplot(2,1,2);plot(data.rt_tout,filtAccel(2,:))
+%     xlabel('Time (sec)');ylabel('Accel (m/s2)');
+%     title(['Y Linear Acceleration, Case: ' num2str(i)])  
+%     
+%     figure;
+%     plot(data.rt_tout,filtRate(3,:)./d2r)
+%     xlabel('Time (sec)');ylabel('Rate (deg/s)');
+%     title(['Rotational Rate, Case:' num2str(i)])
     
-%     thrusterCmds = squeeze(data.rt_yout.signals(1).values(1,:,:))';
+
+    
+   
 %     figure;
 %     subplot(4,1,1);plot(data.rt_tout,thrusterCmds(:,1))
 %     title(['Thruster 1, Case: ' num2str(i)]);xlabel('Time (sec)');ylabel('Cmd (--)')
@@ -148,7 +206,80 @@ for i=1:nCase
 %     title('Thruster 8');xlabel('Time (sec)');ylabel('Cmd (--)')
 end
 
+% Search through the translational cases and record the off-axis
+% acceleration seen for each thruster
+thrusterOffAxis{1} = []; thrusterOffAxis{2} = []; thrusterOffAxis{3} = [];
+thrusterOffAxis{4} = []; thrusterOffAxis{5} = []; thrusterOffAxis{6} = [];
+thrusterOffAxis{7} = []; thrusterOffAxis{8} = [];
+thrusterRotation{1} = []; thrusterRotation{2} = []; thrusterRotation{3} = [];
+thrusterRotation{4} = []; thrusterRotation{5} = []; thrusterRotation{6} = [];
+thrusterRotation{7} = []; thrusterRotation{8} = [];
 
+for i=1:nCase
+    if type(i) == 1 || type(i) == 2
+        for j=1:8
+            if ~isempty(find(cell2mat(thrusterList{j}) == i))
+                thrusterOffAxis{j}{length(thrusterOffAxis{j})+1} = dAccelOff(i);
+                thrusterRotation{j}{length(thrusterRotation{j})+1} = angAccel(i);
+            end
+        end
+    end
+end
+   
+
+
+figure;hold all;
+plot(find(type==1),dAccelOn(typeList{1}),'bo');
+plot(find(type==2),dAccelOn(typeList{2}),'rx');
+plot(find(type==3),dAccelOn(typeList{3}),'g+')
+plot(1:nCase,2*nominalLinAccel*ones(nCase),'k',1:nCase,-2*nominalLinAccel*ones(nCase),'k')
+xlabel('Test Case (-)');ylabel('Linear Acceleration (m/s^2)')
+title('On-Axis Linear Acceleration During Firing')
+legend('X Translation Test','Y Translation Test','Z Rotation Test','Assumed Nominal')
+
+figure;hold all;
+plot(find(type==1),dAccelOff(typeList{1}),'bo');
+plot(find(type==2),dAccelOff(typeList{2}),'rx');
+plot(find(type==3),dAccelOff(typeList{3}),'g+')
+xlabel('Test Case (-)');ylabel('Linear Acceleration (m/s^2)')
+title('Off-Axis Linear Acceleration During Firing')
+legend('X Translation Test','Y Translation Test','Z Rotation Test')
+
+figure;hold all;
+plot(find(type==1),angAccel(typeList{1})./d2r,'bo');
+plot(find(type==2),angAccel(typeList{2})./d2r,'rx');
+plot(find(type==3),angAccel(typeList{3})./d2r,'g+')
+plot(1:nCase,2*nominalAngAccel*ones(nCase)./d2r,'k',1:nCase,-2*nominalAngAccel*ones(nCase)./d2r,'k')
+xlabel('Test Case (-)');ylabel('Angular Acceleration (deg/s^2)')
+title('Angular Acceleration During Firing')
+legend('X Translation Test','Y Translation Test','Z Rotation Test','Assumed Nominal')
+
+
+figure;hold all;
+plot(1*ones(length(thrusterOffAxis{1})),cell2mat(thrusterOffAxis{1}),'o')
+plot(2*ones(length(thrusterOffAxis{2})),cell2mat(thrusterOffAxis{2}),'o')
+plot(3*ones(length(thrusterOffAxis{3})),cell2mat(thrusterOffAxis{3}),'o')
+plot(4*ones(length(thrusterOffAxis{4})),cell2mat(thrusterOffAxis{4}),'o')
+plot(5*ones(length(thrusterOffAxis{5})),cell2mat(thrusterOffAxis{5}),'o')
+plot(6*ones(length(thrusterOffAxis{6})),cell2mat(thrusterOffAxis{6}),'o')
+plot(7*ones(length(thrusterOffAxis{7})),cell2mat(thrusterOffAxis{7}),'o')
+plot(8*ones(length(thrusterOffAxis{8})),cell2mat(thrusterOffAxis{8}),'o')
+xlabel('Thruster');ylabel('Linear Acceleration (m/s^2)')
+title('Off-Axis Linear Acceleration During Translational Firings')
+axis([0 9 -Inf Inf])
+
+figure;hold all;
+plot(1*ones(length(thrusterRotation{1})),cell2mat(thrusterRotation{1})./d2r,'o')
+plot(2*ones(length(thrusterRotation{2})),cell2mat(thrusterRotation{2})./d2r,'o')
+plot(3*ones(length(thrusterRotation{3})),cell2mat(thrusterRotation{3})./d2r,'o')
+plot(4*ones(length(thrusterRotation{4})),cell2mat(thrusterRotation{4})./d2r,'o')
+plot(5*ones(length(thrusterRotation{5})),cell2mat(thrusterRotation{5})./d2r,'o')
+plot(6*ones(length(thrusterRotation{6})),cell2mat(thrusterRotation{6})./d2r,'o')
+plot(7*ones(length(thrusterRotation{7})),cell2mat(thrusterRotation{7})./d2r,'o')
+plot(8*ones(length(thrusterRotation{8})),cell2mat(thrusterRotation{8})./d2r,'o')
+xlabel('Thruster');ylabel('Angular Acceleration (deg/s^2)')
+title('Angular Acceleration During Translational Firings')
+axis([0 9 -Inf Inf])
 return
 % npts = 2000;
 % iAccel(1) = mean(data1.rt_yout.signals(2).values(1:npts/2,1));
